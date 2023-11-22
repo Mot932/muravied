61aad5282f46c2eed7c02c3ea761389787f1e912
DEB <112969476+Mot932@users.noreply.github.com>
2023-11-22 15:35:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
+import keyboard
+import os
+import random
+
 COLS = 25
 ROWS = 10
 EMPTY = '☐'
 PLAYER = 'P'
-ANTHILL = 'A'
 ANT = 'a'
 
-import random
-
-
 class Cell:
-    """
-    Класс, представляющий ячейку в игровом поле.
-
-    Attributes:
-    image (str): Символ, представляющий изображение ячейки (Default '☐').
-    Y (int): Координата по вертикали.
-    X (int): Координата по горизонтали.
-    content: Содержимое ячейки, например, объект игрока.
-    """
-
     def __init__(self, Y=None, X=None):
-        """
-        Инициализация ячейки.
-
-        Parameters:
-        image (str): Символ, представляющий изображение ячейки (Default '☐').
-        Y (int): Координата по вертикали.
-        X (int): Координата по горизонтали.
-        """
         self.image = EMPTY
         self.Y = Y
         self.X = X
-        self.content = None  
+        self.content = None
 
     def draw(self):
         if self.content:
@@ -39,56 +21,116 @@ class Cell:
         else:
             print(self.image, end=' ')
 
-class Player:
-    """
-    Класс, представляющий игрока.
-
-    Attributes:
-    image (str): Символ, представляющий изображение игрока (Default 'P').
-    Y (int): Координата игрока по вертикали.
-    X (int): Координата игрока по горизонтали.
-    """
-
-    def __init__(self, Y=None, X=None):
-        """
-        Инициализация игрока.
-
-        Parameters:
-        image (str): Символ, представляющий изображение игрока (Default 'P').
-        Y (int): Координата игрока по вертикали.
-        X (int): Координата игрока по горизонтали.
-        """
-        self.image = PLAYER
+class Character:
+    def __init__(self, Y=None, X=None, image=None):
+        self.image = image
         self.Y = Y
         self.X = X
 
+    def move(self, direction, field):
+        new_Y, new_X = self.Y, self.X
+
+        if direction == 'up' and self.Y > 0:
+            new_Y -= 1
+        elif direction == 'down' and self.Y < field.rows - 1:
+            new_Y += 1
+        elif direction == 'left' and self.X > 0:
+            new_X -= 1
+        elif direction == 'right' and self.X < field.cols - 1:
+            new_X += 1
+
+        if field.cells[new_Y][new_X].content is None:
+            field.cells[self.Y][self.X].content = None
+            self.Y, self.X = new_Y, new_X
+            field.cells[self.Y][self.X].content = self
+
+class Player(Character):
+    def __init__(self, Y=None, X=None):
+        super().__init__(Y, X, image=PLAYER)
+
+class Ant(Character):
+    def __init__(self, Y=None, X=None):
+        super().__init__(Y, X, image=ANT)
+
+    def move_away_from_player(self, player, field):
+        directions = ['up', 'down', 'left', 'right']
+        opposite_directions = {
+            'up': 'down',
+            'down': 'up',
+            'left': 'right',
+            'right': 'left'
+        }
+
+        direction = random.choice(directions)
+        player_distance = abs(player.Y - self.Y) + abs(player.X - self.X)
+
+        while True:
+            self.move(direction, field)
+
+            new_distance = abs(player.Y - self.Y) + abs(player.X - self.X)
+            if new_distance >= player_distance:
+                break
+
+            direction = opposite_directions[direction]
+
 class Field:
-
-    def __init__(self, cell=Cell, player=Player):
-        """
-        Инициализация игрового поля.
-
-        Parameters:
-        rows (int): Количество строк в поле (Default 10).
-        cols (int): Количество столбцов в поле (Default 25).
-        cell (Cell): Класс ячейки, используемый для создания полей.
-        player (Player): Класс игрока.
-        """
+    def __init__(self, cell=Cell, player=Player, ant=Ant):
         self.rows = ROWS
         self.cols = COLS
         self.cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
-        self.player = player(Y=random.randint(0, ROWS-1), X=random.randint(0, COLS-1))
+        self.player = player(Y=random.randint(0, ROWS - 1), X=random.randint(0, COLS - 1))
+        self.ant = ant(Y=random.randint(0, ROWS - 1), X=random.randint(0, COLS - 1))
         self.cells[self.player.Y][self.player.X].content = self.player
+        self.cells[self.ant.Y][self.ant.X].content = self.ant
 
     def drawrows(self):
-        """
-        Отображение содержимого полей в виде строк.
-        """
         for row in self.cells:
             for cell in row:
                 cell.draw()
             print()
 
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-MyClass = Field()
-MyClass.drawrows()
+class Game:
+    def __init__(self):
+        self.field = Field()
+
+    def handle_keyboard_event(self, event):
+        if event.event_type == keyboard.KEY_DOWN:
+            if event.name == 'up':
+                self.field.player.move('up', self.field)
+                self.field.ant.move_away_from_player(self.field.player, self.field)
+            elif event.name == 'down':
+                self.field.player.move('down', self.field)
+                self.field.ant.move_away_from_player(self.field.player, self.field)
+            elif event.name == 'left':
+                self.field.player.move('left', self.field)
+                self.field.ant.move_away_from_player(self.field.player, self.field)
+            elif event.name == 'right':
+                self.field.player.move('right', self.field)
+                self.field.ant.move_away_from_player(self.field.player, self.field)
+            elif event.name == 'esc':
+                print("Выход из игры.")
+                return True
+            return False
+
+    def update_game_state(self):
+        clear_screen()
+        self.field.drawrows()
+
+    def run(self):
+        self.field.drawrows()
+
+        while True:
+            event = keyboard.read_event(suppress=True)
+            if self.handle_keyboard_event(event):
+                break
+
+            self.update_game_state()
+
+game_instance = Game()
+game_instance.run()
