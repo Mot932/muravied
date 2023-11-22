64686b252914b0c9d193e81f499df4977a88e065
DEB <112969476+Mot932@users.noreply.github.com>
2023-11-22 15:44:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,60 @@ import keyboard
 import os
 import random
 
+# Определение символов для отображения на поле
 COLS = 25
 ROWS = 10
 EMPTY = '☐'
 PLAYER = 'P'
 ANT = 'a'
 
+# Класс, представляющий ячейку на поле
 class Cell:
     def __init__(self, Y=None, X=None):
+        """
+        Конструктор класса Cell.
+
+        Параметры:
+        - Y: Координата Y ячейки.
+        - X: Координата X ячейки.
+        """
         self.image = EMPTY
         self.Y = Y
         self.X = X
         self.content = None
 
     def draw(self):
+        """
+        Метод отрисовки содержимого ячейки.
+        """
         if self.content:
             print(self.content.image, end=' ')
         else:
             print(self.image, end=' ')
 
+# Класс, представляющий персонажа на поле
 class Character:
     def __init__(self, Y=None, X=None, image=None):
+        """
+        Конструктор класса Character.
+
+        Параметры:
+        - Y: Координата Y персонажа.
+        - X: Координата X персонажа.
+        - image: Символ, представляющий персонажа на поле.
+        """
         self.image = image
         self.Y = Y
         self.X = X
 
     def move(self, direction, field):
+        """
+        Метод перемещения персонажа в указанном направлении.
+
+        Параметры:
+        - direction: Направление движения ('up', 'down', 'left', 'right').
+        - field: Объект класса Field, представляющий игровое поле.
+        """
         new_Y, new_X = self.Y, self.X
 
         if direction == 'up' and self.Y > 0:
@@ -44,15 +72,38 @@ class Character:
             self.Y, self.X = new_Y, new_X
             field.cells[self.Y][self.X].content = self
 
+# Класс, представляющий игрока
 class Player(Character):
     def __init__(self, Y=None, X=None):
+        """
+        Конструктор класса Player.
+
+        Параметры:
+        - Y: Координата Y игрока.
+        - X: Координата X игрока.
+        """
         super().__init__(Y, X, image=PLAYER)
 
+# Класс, представляющий муравья
 class Ant(Character):
     def __init__(self, Y=None, X=None):
+        """
+        Конструктор класса Ant.
+
+        Параметры:
+        - Y: Координата Y муравья.
+        - X: Координата X муравья.
+        """
         super().__init__(Y, X, image=ANT)
 
     def move_away_from_player(self, player, field):
+        """
+        Метод перемещения муравья в противоположном направлении относительно игрока.
+
+        Параметры:
+        - player: Объект класса Player, представляющий игрока.
+        - field: Объект класса Field, представляющий игровое поле.
+        """
         directions = ['up', 'down', 'left', 'right']
         opposite_directions = {
             'up': 'down',
@@ -73,8 +124,17 @@ class Ant(Character):
 
             direction = opposite_directions[direction]
 
+# Класс, представляющий игровое поле
 class Field:
     def __init__(self, cell=Cell, player=Player, ant=Ant):
+        """
+        Конструктор класса Field.
+
+        Параметры:
+        - cell: Класс, представляющий ячейку на поле.
+        - player: Класс, представляющий игрока.
+        - ant: Класс, представляющий муравья.
+        """
         self.rows = ROWS
         self.cols = COLS
         self.cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
@@ -84,22 +144,40 @@ class Field:
         self.cells[self.ant.Y][self.ant.X].content = self.ant
 
     def drawrows(self):
+        """
+        Метод отрисовки всех строк игрового поля.
+        """
         for row in self.cells:
             for cell in row:
                 cell.draw()
             print()
 
+# Функция для очистки экрана консоли
 def clear_screen():
+    """
+    Функция для очистки экрана консоли.
+    """
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
+# Класс, представляющий игру
 class Game:
     def __init__(self):
+        """
+        Конструктор класса Game.
+        """
         self.field = Field()
 
+    # Обработка событий клавиатуры
     def handle_keyboard_event(self, event):
+        """
+        Метод обработки событий клавиатуры.
+
+        Параметры:
+        - event: Объект события клавиатуры.
+        """
         if event.event_type == keyboard.KEY_DOWN:
             if event.name == 'up':
                 self.field.player.move('up', self.field)
@@ -118,11 +196,19 @@ class Game:
                 return True
             return False
 
+    # Обновление состояния игры
     def update_game_state(self):
+        """
+        Метод обновления состояния игры.
+        """
         clear_screen()
         self.field.drawrows()
 
+    # Запуск игры
     def run(self):
+        """
+        Метод запуска игры.
+        """
         self.field.drawrows()
 
         while True:
@@ -132,5 +218,6 @@ class Game:
 
             self.update_game_state()
 
+# Создание экземпляра игры и запуск
 game_instance = Game()
 game_instance.run()
